928aaf255e3b6911f8ebceab664fea5ffc362c18
Ondra Pelech <ondra.pelech@gmail.com>
2020-09-25 23:01:46 +0200

    relative import inside

--- src/main/g8/$name;format="space,hyphen,lower"$/shared/src/test/scala/$name;format="space,lower,package,packaged"$/HelloWorldSpec.scala
+++ src/main/g8/$name;format="space,hyphen,lower"$/shared/src/test/scala/$name;format="space,lower,package,packaged"$/HelloWorldSpec.scala
@@ -6,14 +6,15 @@ import zio.test.Assertion._
 import zio.test._
 import zio.test.environment._
 
-import HelloWorld._
-
 object HelloWorld {
   def sayHello: ZIO[Console, Nothing, Unit] =
     console.putStrLn("Hello, World!")
 }
 
 object HelloWorldSpec extends DefaultRunnableSpec {
+
+  import HelloWorld._
+
   def spec: ZSpec[Environment, Failure] = suite("HelloWorldSpec")(
     testM("sayHello correctly displays output") {
       for {
