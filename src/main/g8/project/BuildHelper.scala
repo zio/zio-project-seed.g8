fc5bd41c8fc495efad9394ffeb4d5fff3ccd09fc
Ondra Pelech <ondra.pelech@gmail.com>
2020-09-25 22:04:57 +0200

    Escape $ (#28)
    * Escape $
    
    * Escape all $ in BuildHelper.scala
    
    * Escape all $ in circleci/config.yml
    
    * Escape all $ in auto-approve.yml
    
    * Make sure it compiles and passes tests

--- src/main/g8/project/BuildHelper.scala
+++ src/main/g8/project/BuildHelper.scala
@@ -212,14 +212,14 @@ object BuildHelper {
   )
 
   def stdSettings(prjName: String) = Seq(
-    name := s"$prjName",
+    name := s"\$prjName",
     crossScalaVersions := Seq(Scala211, Scala212, Scala213),
     scalaVersion in ThisBuild := Scala212,
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value, optimize = !isSnapshot.value),
     libraryDependencies ++= {
       if (isDotty.value)
         Seq(
-          ("com.github.ghik" % s"silencer-lib_$Scala213" % SilencerVersion % Provided)
+          ("com.github.ghik" % s"silencer-lib_\$Scala213" % SilencerVersion % Provided)
             .withDottyCompat(scalaVersion.value)
         )
       else
@@ -342,28 +342,28 @@ object BuildHelper {
   def welcomeMessage = onLoadMessage := {
     import scala.Console
 
-    def header(text: String): String = s"${Console.RED}$text${Console.RESET}"
+    def header(text: String): String = s"\${Console.RED}\$text\${Console.RESET}"
 
-    def item(text: String): String    = s"${Console.GREEN}> ${Console.CYAN}$text${Console.RESET}"
-    def subItem(text: String): String = s"  ${Console.YELLOW}> ${Console.CYAN}$text${Console.RESET}"
+    def item(text: String): String    = s"\${Console.GREEN}> \${Console.CYAN}\$text\${Console.RESET}"
+    def subItem(text: String): String = s"  \${Console.YELLOW}> \${Console.CYAN}\$text\${Console.RESET}"
 
-    s"""|${header(" ________ ___")}
-        |${header("|__  /_ _/ _ \\")}
-        |${header("  / / | | | | |")}
-        |${header(" / /_ | | |_| |")}
-        |${header(s"/____|___\\___/   ${version.value}")}
+    s"""|\${header(" ________ ___")}
+        |\${header("|__  /_ _/ _ \\\\")}
+        |\${header("  / / | | | | |")}
+        |\${header(" / /_ | | |_| |")}
+        |\${header(s"/____|___\\\\___/   \${version.value}")}
         |
         |Useful sbt tasks:
-        |${item("build")} - Prepares sources, compiles and runs tests.
-        |${item("prepare")} - Prepares sources by applying both scalafix and scalafmt
-        |${item("fix")} - Fixes sources files using scalafix
-        |${item("fmt")} - Formats source files using scalafmt
-        |${item("~compileJVM")} - Compiles all JVM modules (file-watch enabled)
-        |${item("testJVM")} - Runs all JVM tests
-        |${item("testJS")} - Runs all ScalaJS tests
-        |${item("testOnly *.YourSpec -- -t \"YourLabel\"")} - Only runs tests with matching term e.g.
-        |${subItem("coreTestsJVM/testOnly *.ZIOSpec -- -t \"happy-path\"")}
-        |${item("docs/docusaurusCreateSite")} - Generates the ZIO microsite
+        |\${item("build")} - Prepares sources, compiles and runs tests.
+        |\${item("prepare")} - Prepares sources by applying both scalafix and scalafmt
+        |\${item("fix")} - Fixes sources files using scalafix
+        |\${item("fmt")} - Formats source files using scalafmt
+        |\${item("~compileJVM")} - Compiles all JVM modules (file-watch enabled)
+        |\${item("testJVM")} - Runs all JVM tests
+        |\${item("testJS")} - Runs all ScalaJS tests
+        |\${item("testOnly *.YourSpec -- -t \"YourLabel\"")} - Only runs tests with matching term e.g.
+        |\${subItem("coreTestsJVM/testOnly *.ZIOSpec -- -t \"happy-path\"")}
+        |\${item("docs/docusaurusCreateSite")} - Generates the ZIO microsite
       """.stripMargin
   }
 
